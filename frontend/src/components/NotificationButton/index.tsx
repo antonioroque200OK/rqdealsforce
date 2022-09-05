95266a4b295ae5e1e16b3e44e67b8e6e1b109930
antonio <antoniocarlos.roque@gmail.com>
2022-09-05 20:57:30 -0300

    Started implmentation of NotificationButton component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,9 @@
+import icon from "../../assets/images/notification-icon.svg"
+
+export function NotificationButton() {
+    return (
+        <div className="sales-red-btn">
+            <img src={ icon } alt="notify" />
+        </div>
+    );
+}
