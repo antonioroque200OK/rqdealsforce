e27acd2525eee25813cd50d8e0705dc46936bcb0
antonio <antoniocarlos.roque@gmail.com>
2022-09-09 15:06:48 -0300

    Improved message content sent through SMS

--- backend/rqdealsforce/src/main/java/com/roqueantonio/rqdealsforce/services/SmsService.java
+++ backend/rqdealsforce/src/main/java/com/roqueantonio/rqdealsforce/services/SmsService.java
@@ -1,8 +1,13 @@
 package com.roqueantonio.rqdealsforce.services;
 
+import com.roqueantonio.rqdealsforce.entities.Sale;
+import com.roqueantonio.rqdealsforce.repositories.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,14 +17,25 @@ public class SmsService {
 
     public static final String AUTH_TOKEN = System.getenv("TWILIO_KEY");
 
-    public void sendSms() {
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public void sendSms(Long saleId) {
+        Sale sale = saleRepository.findById(saleId).get();
+
+        String date = sale.getDate().getMonthValue() + "-" + sale.getDate().getDayOfMonth() + "-"
+                + sale.getDate().getYear();
+
+        String message = "Seller " + sale.getSellerName() + " closed deal in " +
+                date + " with value of " + String.format("%.2f", sale.getAmount());
+
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-        Message message = Message.creator(
+        Message sellNotification = Message.creator(
                 new com.twilio.type.PhoneNumber("+5598987058414"),
                 new com.twilio.type.PhoneNumber("+19853324345"),
-                "TEST").create();
+                message).create();
 
-        System.out.println(message.getSid());
+        System.out.println(sellNotification.getSid());
     }
 }
